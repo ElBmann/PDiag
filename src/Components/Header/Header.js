309db5b45f33fe221668026f7c5bd80e8ce88367
brianrecuero <brianrecuero@gmail.com>
2018-06-23 12:19:15 -0400

    Added ReactStrap

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+import { Button } from 'reactstrap';
+const header = (props) => {
+    return(
+        <Button color="danger">Danger!</Button>
+    )
+};
+
+export default header;
