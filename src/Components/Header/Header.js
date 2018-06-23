2c5dc089109a8b36d094cbddb88557b0d2a554a5
brianrecuero <brianrecuero@gmail.com>
2018-06-23 13:43:23 -0400

    Added Correct Names for nav bar

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
+import '../Header/Header.css'
 import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem,
     UncontrolledDropdown, DropdownToggle, DropdownItem,
     NavLink, DropdownMenu
@@ -8,20 +9,36 @@ import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem,
 const header = (props) => {
     return(
         <div>
-        <Navbar color="light" light expand="md">
-          <NavbarBrand href="/">reactstrap</NavbarBrand>
+        <Navbar color="dark" dark expand="md">
+          <NavbarBrand className="text-white" href="/">Percise Diagnostics</NavbarBrand>
           <NavbarToggler onClick={this.toggle} />
           <Collapse navbar>
-            <Nav className="ml-auto" navbar>
+            <Nav  className="ml-auto" navbar >
+              <NavItem color="primary" >
+                <NavLink  className="text-white" href="/components/" ></NavLink>
+              </NavItem>
+              <NavItem >
+                <NavLink className="text-white"  href="/components/">Services</NavLink>
+              </NavItem>
+              
               <NavItem>
-                <NavLink href="/components/">Components</NavLink>
+                <NavLink className="text-white" href="/components/">Our Pathologists</NavLink>
               </NavItem>
               <NavItem>
-                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+                <NavLink className="text-white" href="/components/">Physicians & Patients</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="/components/">Billing</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="/components/">Contact</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink className="text-white" href="https://github.com/reactstrap/reactstrap">Careers</NavLink>
               </NavItem>
               <UncontrolledDropdown nav inNavbar>
-                <DropdownToggle nav caret>
-                  Options
+                <DropdownToggle className="text-white" nav caret>
+                Pathology Reporting
                 </DropdownToggle>
                 <DropdownMenu right>
                   <DropdownItem>
