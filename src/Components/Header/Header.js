4bc557e4e73e25379e110d846ec0b1e81f7e014c
brianrecuero <brianrecuero@gmail.com>
2018-06-23 12:28:57 -0400

    Header Style Created

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,9 +1,45 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
-import { Button } from 'reactstrap';
+import { Navbar, NavbarBrand, NavbarToggler, Collapse, Nav, NavItem,
+    UncontrolledDropdown, DropdownToggle, DropdownItem,
+    NavLink, DropdownMenu
+
+} from 'reactstrap';
 const header = (props) => {
     return(
-        <Button color="danger">Danger!</Button>
+        <div>
+        <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">reactstrap</NavbarBrand>
+          <NavbarToggler onClick={this.toggle} />
+          <Collapse navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink href="/components/">Components</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink href="https://github.com/reactstrap/reactstrap">GitHub</NavLink>
+              </NavItem>
+              <UncontrolledDropdown nav inNavbar>
+                <DropdownToggle nav caret>
+                  Options
+                </DropdownToggle>
+                <DropdownMenu right>
+                  <DropdownItem>
+                    Option 1
+                  </DropdownItem>
+                  <DropdownItem>
+                    Option 2
+                  </DropdownItem>
+                  <DropdownItem divider />
+                  <DropdownItem>
+                    Reset
+                  </DropdownItem>
+                </DropdownMenu>
+              </UncontrolledDropdown>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </div>
     )
 };
 
